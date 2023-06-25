a01ad85cc15ee6d06bb45344c783b5f8d8331021
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2023-06-25 19:54:56 -0300

    Definindo os endereços que a stack vai tratar
    Definindo os endereços que a stack fetch e admin irão tratar.
    A fetch apenas pelos relacionados a leitura e a Admin as que precisam escrever na tabela do dynamoDB

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -7,6 +7,7 @@ import { Construct } from "constructs"
 //para não ter de ficar adicionando vários parametros, criamos uma interface que vai ter os dados que precisamos, é um objeto com todos os parametros que queremos
 interface ECommerceApiStackProps extends cdk.StackProps {
     productsFetchHandler: lambdaNodeJS.NodejsFunction
+    productsAdminHandler: lambdaNodeJS.NodejsFunction
 }
 
 export class ECommerceApiStack extends cdk.Stack {
@@ -37,8 +38,23 @@ export class ECommerceApiStack extends cdk.Stack {
         //aqui usamos os parametros passado pelo props, no caso um meio de apontar para a stack de produtos
         const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
 
-        // "/products"
+        // GET "/products"
         const productsResource = api.root.addResource("products")
         productsResource.addMethod("GET", productsFetchIntegration)
+
+        // GET /products/{id}
+        const productIdResource = productsResource.addResource("{id}")
+        productIdResource.addMethod("GET", productsFetchIntegration)
+
+        const productsAdminIntegration = new apigateway.LambdaIntegration(props.productsAdminHandler)
+
+        // POST /products
+        productsResource.addMethod("POST", productsAdminIntegration)
+
+        // PUT /products/{id}
+        productIdResource.addMethod("PUT", productsAdminIntegration)
+
+        // DELETE /products/{id}
+        productIdResource.addMethod("DELETE", productsAdminIntegration)
     }
 }
