7a6d2dbf539ad245f416424728973bdeaec19e62
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2023-08-01 21:15:08 -0300

    Adicionando função e comentários diversos
    função de buscar produto por id

--- lambda/products/layers/productsLayer/nodejs/productRepository.ts
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -18,10 +18,27 @@ export class ProductRepository {
         this.productsDdb = productsDdb;
     }
 
+    //Irá retornar todos os produtos cadastrados no banco de dados
     async getAllProducts(): Promise<Product[]> {
         const data = await this.ddbClient.scan({
             TableName: this.productsDdb
         }).promise()
-        return data.Items as Product[]
+        return data.Items as Product[] //Transformei a resposta em uma lista de Produtos
+    }
+
+    //Irá retornar um produto que tenha o id solicitado, caso não encontre irá lançar um erro
+    async getProductById(productId: string): Promise<Product> {
+        const data = await this.ddbClient.get({
+            TableName: this.productsDdb,
+            Key: {
+                id: productId //Tem de ser o mesmo nome que está no banco de dados 
+            }
+        }).promise()
+        //Retornando ou não um produto, terá um resultado diferente
+        if (data.Item) {
+            return data.Item as Product
+        } else {
+            throw new Error('Product not found')
+        }
     }
 }
