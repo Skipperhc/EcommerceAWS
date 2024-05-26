0178f7916236c48517cbc21120d3735d4ef04b63
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2024-05-25 21:40:27 -0300

    Criando o código da função de geração de eventos de alteração de produtos
    Agora a lambda de eventos está salvando no banco de dados as informações que queremos

---
+++ lambda/products/productEventsFunction.ts
@@ -0,0 +1,47 @@
+import { Callback, Context } from "aws-lambda";
+import { ProductEvent } from "./layers/productEventsLayer/nodejs/productEvent";
+import { DynamoDB } from "aws-sdk";
+import * as AWSXRAY from "aws-xray-sdk"
+
+
+//para evitar atrasos ou erros, tudo que pode ser iniciado antes do handler vai ser iniciado aqui
+AWSXRAY.captureAWS(require("aws-sdk"))
+
+const eventsDdb = process.env.EVENTS_DDB!
+const ddbClient = new DynamoDB.DocumentClient()
+
+export async function handler(event: ProductEvent, context: Context, callback: Callback): Promise<void> {
+    //TODO - to be removed
+    console.log(event)
+
+    console.log(`Lambda requestId: ${context.awsRequestId}`)
+    
+    await createEvent(event)
+
+    callback(null, JSON.stringify({
+        productEventCreated: true,
+        message: "OK"
+    }))
+
+}
+
+function createEvent(event: ProductEvent) {
+    const timestamp = Date.now()
+    const ttl = ~~(timestamp / 1000 + 5 + 60) //5 min a frente do timestamp
+    return ddbClient.put({
+        TableName: eventsDdb,
+        Item: {
+            pk: `#product_${event.productCode}`,
+            sk: `${event.eventType}#${timestamp}`, //PRODUCT_CREATED#123456
+            email: event.email,
+            createdAt: timestamp,
+            requestId: event.requestId,
+            eventType: event.eventType,
+            info: {
+                productId: event.productId,
+                price: event.productPrice
+            },
+            ttl: ttl
+        }
+    }).promise()
+}
