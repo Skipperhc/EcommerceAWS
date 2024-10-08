110b6129747182ec254c46186cef8a69110185f5
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2024-10-08 20:43:41 -0300

    Criando uma role para a lambda de orderEvents
    Restringindo o acesso a tabela de events, a lambda orderEvents pode somente editar

--- lib/ordersApp-stack.ts
+++ lib/ordersApp-stack.ts
@@ -5,6 +5,7 @@ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 import * as ssm from 'aws-cdk-lib/aws-ssm'
 import * as sns from "aws-cdk-lib/aws-sns"
 import * as subs from "aws-cdk-lib/aws-sns-subscriptions"
+import * as iam from "aws-cdk-lib/aws-iam"
 import { Construct } from 'constructs'
 
 interface OrdersAppStackProps extends cdk.StackProps {
@@ -16,7 +17,6 @@ export class OrdersAppStack extends cdk.Stack {
     readonly ordersHandler: lambdaNodeJS.NodejsFunction
     constructor(scope: Construct, id: string, props: OrdersAppStackProps) {
         super(scope, id, props)
-
 
         const ordersDdb = new dynamodb.Table(this, "ordersDdb", {
             tableName: "orders",
@@ -104,5 +104,16 @@ export class OrdersAppStack extends cdk.Stack {
         })
 
         ordersTopic.addSubscription(new subs.LambdaSubscription(orderEventsHandler))
+        const eventsDdbPolicy = new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            actions: ["dynamodb:PutItem"],
+            resources: [props.eventsDdb.tableArn],
+            conditions: {
+                ['ForAllValues:StringLike']: {
+                    'dynamodb:LeadingKeys': ['#order_*']
+                }
+            }
+        })
+        orderEventsHandler.addToRolePolicy(eventsDdbPolicy)
     }
 }
