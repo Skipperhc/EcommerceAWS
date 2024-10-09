bf404436a01ec4e55012185caca05f98bb84a735
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2024-10-09 17:01:55 -0300

    ops

--- lambda/orders/layers/orderEventsRepositoryLayer/nodejs/orderEventRepository.ts
+++ lambda/orders/layers/orderEventsRepositoryLayer/nodejs/orderEventRepository.ts
@@ -10,7 +10,7 @@ export interface OrderEventDdb {
     eventType: string;
     info: {
         orderId: string;
-        productCodes: string;
+        productCodes: string[];
         messageId: string;
     }
 }
