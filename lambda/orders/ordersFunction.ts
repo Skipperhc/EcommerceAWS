c440313e89316d5ae1a2ce6c54d1e3a6b6debb0a
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2024-10-10 14:13:01 -0300

    alterando lambda para rodar processos em paralelo

--- lambda/orders/ordersFunction.ts
+++ lambda/orders/ordersFunction.ts
@@ -5,6 +5,8 @@ import * as AWSXRay from "aws-xray-sdk"
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda"
 import { CarrierType, OrderProductResponse, OrderRequest, OrderResponse, PaymentType, ShippingType } from "/opt/nodejs/ordersApiLayer"
 import { OrderEvent, OrderEventType, Envelope } from "/opt/nodejs/orderEventsLayer"
+import { v4 as uuid } from "uuid"
+
 
 AWSXRay.captureAWS(require("aws-sdk"))
 
@@ -69,18 +71,20 @@ export async function handler(event: APIGatewayProxyEvent, context: Context): Pr
         const products = await productRepository.getProductsByIds(orderRequest.productIds)
         if (products.length === orderRequest.productIds.length) {
             const order = buildOrder(orderRequest, products)
-            const orderCreated = await orderRepository.createOrder(order)
+            const orderCreatedPromise = orderRepository.createOrder(order)
 
-            const eventResult = await sendOrderEvent(orderCreated, OrderEventType.CREATED, lambdaRquestId)
+            const eventResultPromise = sendOrderEvent(order, OrderEventType.CREATED, lambdaRquestId)
+
+            const results = await Promise.all([orderCreatedPromise, eventResultPromise])
 
             console.log(
-                `Order created event sent - OrderId: ${orderCreated.sk}
-                - MessageId: ${eventResult.MessageId}`
+                `Order created event sent - OrderId: ${order.sk}
+                - MessageId: ${results[1].MessageId}`
             )
 
             return {
                 statusCode: 201,
-                body: JSON.stringify(convertToOrderResponse(orderCreated))
+                body: JSON.stringify(convertToOrderResponse(order))
             }
         } else {
             return {
@@ -191,6 +195,8 @@ function buildOrder(orderRequest: OrderRequest, products: Product[]): Order {
 
     const order: Order = {
         pk: orderRequest.email,
+        sk: uuid(),
+        createdAt: Date.now(),
         billing: {
             payment: orderRequest.payment,
             totalPrice: totalPrice
