34f2f38026f39459ead3d2cbe0b49ea65dbbea32
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2024-10-10 18:46:01 -0300

    Para identificar o log

--- lambda/orders/billingFunction.ts
+++ lambda/orders/billingFunction.ts
@@ -2,7 +2,7 @@ import { SNSEvent, Context } from "aws-lambda"
 
 export async function handler(event: SNSEvent, context: Context): Promise<void> {
     event.Records.forEach((record) => {
-        console.log(record.Sns)
+        console.log("Função billing invocada com sucesso", record.Sns)
     })
 
     return 
