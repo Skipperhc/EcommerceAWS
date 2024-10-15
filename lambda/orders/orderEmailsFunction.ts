06cd574929e66171fbd68e96fbe9c85b4209618d
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2024-10-15 17:37:35 -0300

    ops

--- lambda/orders/orderEmailsFunction.ts
+++ lambda/orders/orderEmailsFunction.ts
@@ -1,7 +1,7 @@
 import { Context, SQSEvent } from "aws-lambda";
 import * as AWSXRay from "aws-xray-sdk"
 
-AWSXRay.captureAWS(require("aws-cdk"))
+AWSXRay.captureAWS(require("aws-sdk"))
 
 export async function handler(event: SQSEvent, context: Context): Promise<void> {
 
