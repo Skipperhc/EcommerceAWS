b45bb9c3ca4e1100721ff767767f4c120fefe147
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2024-10-30 05:59:28 -0300

    202. Função Lambda de geração da URL pré-assinada e da transação

--- lambda/invoices/invoiceGetUrlFunction.ts
+++ lambda/invoices/invoiceGetUrlFunction.ts
@@ -1,12 +1,42 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda"
+import { ApiGatewayManagementApi, DynamoDB, S3 } from "aws-sdk"
+import {v4 as uuid } from "uuid"
+
 import * as AWSXRay from "aws-xray-sdk"
 
 AWSXRay.captureAWS(require("aws-sdk"))
 
+const invoicesDdb = process.env.INVOICE_DDB!
+const bucketName = process.env.BUCKET_NAME! 
+const invoicesWsApiEndpoint = process.env.INVOICE_WSAPI_ENDPOINT!.substring(6)
+
+const s3Client = new S3()
+const ddbClient = new DynamoDB.DocumentClient()
+const apigwManagementApi = new ApiGatewayManagementApi({
+    endpoint: invoicesWsApiEndpoint
+})
+
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
-    
+
+    //TODO - to be removed
     console.log(event)
     
+    const lambdaRequestId = context.awsRequestId
+    const connectionId = event.requestContext.connectionId!
+
+    console.log(`ConnectionId: ${connectionId} - Lambda RequestId: ${lambdaRequestId}`)
+
+    const key = uuid()
+    const expires = 300
+    
+    const signedUrlPut = s3Client.getSignedUrlPromise("putObject", {
+        Bucket: bucketName,
+        Key: key,
+        Expires: expires
+    })
+
+
+
     return {
         statusCode: 200,
         body: "OK"
