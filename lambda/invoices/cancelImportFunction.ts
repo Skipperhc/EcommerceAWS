c55fb4765ad88f7a5f6ff97aadce0f959a4a5272
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2024-11-04 05:03:58 -0300

    211. Função Lambda para o cancelamento da importação do arquivo
    Criando função de cancelamento da importação

--- lambda/invoices/cancelImportFunction.ts
+++ lambda/invoices/cancelImportFunction.ts
@@ -1,12 +1,50 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda"
+import { ApiGatewayManagementApi, DynamoDB } from "aws-sdk"
 import * as AWSXRay from "aws-xray-sdk"
+import { InvoiceTransactionRepository, InvoiceTransactionStatus } from "/opt/nodejs/invoiceTransaction"
+import { InvoiceWSService } from "/opt/nodejs/invoiceWSConnection"
 
 AWSXRay.captureAWS(require("aws-sdk"))
 
+//Os clientes que iremos usar
+const invoicesDdb = process.env.INVOICE_DDB!
+const invoicesWsApiEndpoint = process.env.INVOICE_WSAPI_ENDPOINT!.substring(6)
+
+//Importando os layers que usaremos nessa lambda
+const ddbClient = new DynamoDB.DocumentClient()
+const apigwManagementApi = new ApiGatewayManagementApi({
+    endpoint: invoicesWsApiEndpoint
+})
+
+//Importando os layers que usaremos nessa lambda
+const invoiceTransactionRepository = new InvoiceTransactionRepository(ddbClient, invoicesDdb)
+const invoiceWSService = new InvoiceWSService(apigwManagementApi)
+
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
-    
-    console.log(event)
-    
+
+    const transactionId = JSON.parse(event.body!).trasactionId as string
+    const lambdaRequestId = context.awsRequestId
+    const connectionId = event.requestContext.connectionId!
+
+    console.log(`ConnectionId: ${connectionId} - Lambda RequestId: ${lambdaRequestId}`)
+
+    try {
+        const invoiceTransaction = await invoiceTransactionRepository.getInvoiceTransaction(transactionId)
+        if (invoiceTransaction.transactionStatus === InvoiceTransactionStatus.GENERATED) {
+            await Promise.all([
+                invoiceWSService.sendInvoiceStatus(transactionId, connectionId, InvoiceTransactionStatus.CANCELLED),
+                invoiceTransactionRepository.updateInvoiceTransaction(transactionId, InvoiceTransactionStatus.CANCELLED)
+            ])
+        } else {
+            await invoiceWSService.sendInvoiceStatus(transactionId, connectionId, invoiceTransaction.transactionStatus)
+            console.error(`Cant cancel an ongoing process`)
+        }
+    } catch (error) {
+        console.error((<Error>error).message)
+        console.error(`Invoice transaction not found - transactionId: ${transactionId}`)
+        await invoiceWSService.sendInvoiceStatus(transactionId, connectionId, InvoiceTransactionStatus.NOT_FOUND)
+    }
+
     return {
         statusCode: 200,
         body: "OK"
