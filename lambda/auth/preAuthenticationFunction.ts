56663f5615b8bb76bda339b16e3c641132fddd32
Vitor Hainosz Cardoso <55635014+Skipperhc@users.noreply.github.com>
2025-07-26 18:56:21 -0300

    Bloqueando um email por algum motivo qualquer

--- lambda/auth/preAuthenticationFunction.ts
+++ lambda/auth/preAuthenticationFunction.ts
@@ -3,5 +3,10 @@ import { Callback, Context, PreAuthenticationTriggerEvent } from "aws-lambda";
 export async function handler(event: PreAuthenticationTriggerEvent, context: Context, callback: Callback): Promise<void> {
     console.log(event)
     
-    callback(null, event)
+    //verificando se o usuário que está tentando se logar tem alguma pendencia de qualquer tipo
+    if(event.request.userAttributes.email === "vitorhainosz2@gmail.com") {
+        callback("This user is blocked. Reason: PAYMENT", event)
+    } else {
+        callback(null, event)
+    }
 }
